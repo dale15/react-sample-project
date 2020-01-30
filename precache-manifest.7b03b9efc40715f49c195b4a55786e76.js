1d5d0bcb69fc8413fce3f72ab7feba8cfd9b1a2b
dale <josephdalebertcajida@gmail.com>
2020-01-30 18:39:49 +0800

    Updates

---
+++ precache-manifest.7b03b9efc40715f49c195b4a55786e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381fbfc49a5e00e595cca9d09dd7b05c",
+    "url": "/react-sample-project/index.html"
+  },
+  {
+    "revision": "2cf5f1fac2c9e7cdd9e6",
+    "url": "/react-sample-project/static/css/main.4e4fdc67.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-sample-project/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-sample-project/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cf5f1fac2c9e7cdd9e6",
+    "url": "/react-sample-project/static/js/main.6c4803a2.chunk.js"
+  },
+  {
+    "revision": "d1dd9f7b43e635554c5a",
+    "url": "/react-sample-project/static/js/runtime-main.7bc1bdbf.js"
+  }
+]);
